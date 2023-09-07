2d0041a0796f94cce120429d6b390dd5cfceae62
kailous <kailous@live.cn>
2023-09-08 00:15:27 +0800

    版本 1.9.1 修复vercel部署遇到的问题

--- pages/components/Info.jsx
+++ pages/components/Info.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './css/Info.module.css'; // 使用相对路径导入CSS文件
 
-const Info = ({bookInfo}) => {
+const Info = ({ bookInfo }) => {
+    // 使用条件运算符检查 bookInfo 是否存在，如果不存在则创建一个空对象
+    const bookData = bookInfo ? bookInfo : { book_info: {} };
+
     return (
         <div className={styles.info}>
             <Image
-                src={bookInfo.book_info.audio_thumb_uri}
+                src={bookData.book_info.audio_thumb_uri}
                 alt="书籍封面"
                 width="128" // 根据需要设置宽度
                 height="128" // 根据需要设置高度
             />
             <div className={styles.text}>
                 <div className={styles.title}>
-                    <h1>{bookInfo.book_info.book_name}</h1>
-                    <h3>作者: {bookInfo.book_info.author}</h3>
+                    <h1>{bookData.book_info.book_name}</h1>
+                    <h3>作者: {bookData.book_info.author}</h3>
                 </div>
                 <p dangerouslySetInnerHTML={{
-                    __html: bookInfo.book_info.abstract.replace(/\n　　/g, ' '),
+                    __html: bookData.book_info.abstract ? bookData.book_info.abstract.replace(/\n　　/g, ' ') : '',
                 }}
                 />
                 {/* 添加其他书籍信息字段 */}
--- pages/components/Menu.jsx
+++ pages/components/Menu.jsx
@@ -1,11 +1,13 @@
-// components/Menu.jsx
-
 import React from 'react';
 import styles from './css/Menu.module.css'; // 引入css文件
+
 const Menu = ({ itemDataList }) => {
+    // 使用条件运算符检查 itemDataList 是否存在，如果不存在则返回一个空数组
+    const items = itemDataList ? itemDataList : [];
+
     return (
         <ul className={styles.menu}>
-            {itemDataList.map((item) => (
+            {items.map((item) => (
                 <li key={item.item_id}>
                     <a href={`/chapter?itemId=${item.item_id}`}>{item.title}</a>
                     <span>共计{item.chapter_word_number}字</span>
