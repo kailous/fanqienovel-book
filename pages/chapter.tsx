11516aab69229b63f10631978aa2bb5aaa5f0081
kailous <kailous@live.cn>
2023-09-10 00:00:21 +0800

    版本 2.2.4 修复部署的错误

--- pages/chapter.tsx
+++ pages/chapter.tsx
@@ -5,7 +5,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
-import styles from './styles/Chapter.module.css'
+import styles from './styles/chapter.module.css'
 
 // 导入组件
 import TopBar from './components/TopBar'; // 顶部栏
