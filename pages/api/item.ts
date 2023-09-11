fe51d96d160215714ca44dcc8f21709808c260cb
kailous <kailous@live.cn>
2023-09-11 17:07:32 +0800

    版本 2.3.2 修复多余的空行问题

--- pages/api/item.ts
+++ pages/api/item.ts
@@ -2,6 +2,25 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import cheerio from 'cheerio';
+
+const stripHtmlTagsAndNewlines = (html: string) => {
+    // 移除 HTML 标签并将多余的连续换行符替换为单个换行符
+    let filteredText = html.replace(/<\/?[^>]+(>|$)/g, '').replace(/\n+/g, '\n').replace(/\n\s*\n/g, '\n');
+
+    // 去掉开头的换行符
+    if (filteredText.startsWith('\n')) {
+        filteredText = filteredText.substring(1);
+    }
+
+    // 在最后添加一个换行符
+    if (!filteredText.endsWith('\n')) {
+        filteredText += '\n';
+    }
+
+    return filteredText;
+};
+
 
 const itemHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'GET') {
@@ -16,11 +35,15 @@ const itemHandler = async (req: NextApiRequest, res: NextApiResponse) => {
                 // 提取所需的章节内容和相关信息
                 const chapterData = response.data.data.data;
 
+                // 使用 cheerio 解析 HTML 内容并过滤掉标签和多余的换行符
+                const $ = cheerio.load(chapterData.content);
+                const filteredContent = stripHtmlTagsAndNewlines($.text());
+
                 // 构建返回的数据结构
                 const responseData = {
                     data: {
                         data: {
-                            content: chapterData.content,
+                            content: filteredContent,
                             novel_data: {
                                 author: chapterData.novel_data.author,
                                 abstract: chapterData.novel_data.abstract,
