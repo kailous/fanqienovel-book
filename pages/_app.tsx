e36af5d89e021f9d4a091240a58a3abfb7efd284
kailous <kailous@live.cn>
2023-09-25 22:09:44 +0800

    版本 2.4.1 增加数据分析，分析访客数据。 删除多余的 src目录

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 // pages/_app.js
 
-import '../src/styles/globals.css'; // 引入全局样式
+import './styles/globals.css'; // 引入全局样式
 import { Analytics } from '@vercel/analytics/react';
 function MyApp({Component, pageProps}: { Component: any; pageProps: any }) {
     return (
