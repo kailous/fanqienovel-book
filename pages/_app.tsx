ff044799b70c85e85ca86392b2787c3a752c86a8
kailous <kailous@live.cn>
2023-09-25 11:35:44 +0800

    版本 2.4 增加数据分析，分析访客数据。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 // pages/_app.js
 
 import '../src/styles/globals.css'; // 引入全局样式
-
-function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
-  return <Component {...pageProps} />;
+import { Analytics } from '@vercel/analytics/react';
+function MyApp({Component, pageProps}: { Component: any; pageProps: any }) {
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
 
 export default MyApp;
